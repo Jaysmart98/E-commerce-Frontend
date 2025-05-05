f11f597eb243f4b957e011261e39dde4843568bf
Jaysmart <ogunbunmijoshua60@gmail.com>
2025-05-05 13:34:32 +0100

    feat: add Footer component and update Home page styles for improved layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
     </div>
 
     <div className="carousel-item">
-      <img src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1745790801/Gadgets_mfwk9o.png" class="d-block w-100" alt="..."/>
+      <img id="cardIMG" src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1745790801/Gadgets_mfwk9o.png" class="d-block w-100" alt="..."/>
       <div className="carousel-caption d-none d-md-block">
         <h5>Gadgets</h5>
         <p>Use the search button above to check all sorts of gadgets available.</p>
@@ -80,7 +80,7 @@ const Home = () => {
       <div id="crossSpan">
        <div id="">
        <span className="card-text text-body-secondary">$1,500     </span> 
-       <span id="crosstext2" className="card-text text-body-secondary">   $2,000</span>
+       <span id="crosstext2" className="card-text text-body-secondary">   </span>
        </div>
 
        <div id="crossbtn-container">
@@ -100,7 +100,7 @@ const Home = () => {
       <div id="crossSpan">
        <div id="">
        <span className="card-text text-body-secondary">$50     </span> 
-       <span id="crosstext2" className="card-text text-body-secondary">   $120</span>
+       <span id="crosstext2" className="card-text text-body-secondary">  </span>
        </div>
 
        <div id="crossbtn-container">
@@ -120,7 +120,7 @@ const Home = () => {
       <div id="crossSpan">
        <div id="">
        <span className="card-text text-body-secondary">$900     </span> 
-       <span id="crosstext2" className="card-text text-body-secondary">   $1,200</span>
+       <span id="crosstext2" className="card-text text-body-secondary">  </span>
        </div>
 
        <div id="crossbtn-container">
@@ -131,104 +131,220 @@ const Home = () => {
     </div>
   </div>
 </div>
-      <div className="card-group">
-<div className="card">
-<img src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1745790801/Gadgets_mfwk9o.png" className="card-img-top" alt="..."/>
+
+<div className="card-group">
+<div className="card card-2">
+<img id="cardIMG"  src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1746439089/jacket_yuyxnb.webp" className="card-img-top" alt="..."/>
 <div className="card-body">
-<h5 className="card-title">GADGETS</h5>
-<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+<h5 className="card-title">JACKET</h5>
+<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
 
 <div id="crossSpan">
 <div id="">
 <span className="card-text text-body-secondary">$1,500     </span> 
-<span id="crosstext2" className="card-text text-body-secondary">   $2,000</span>
+<span id="crosstext2" className="card-text text-body-secondary">   </span>
 </div>
 
 <div id="crossbtn-container">
-<button id="crossbtn">Add to Cart</button>
+<button id="crossbtn2">Add to Cart</button>
 </div>
 </div>
 
 </div>
 </div>
 
-<div className="card">
-<img src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1745790350/food_korgga.png" className="card-img-top" alt="..."/>
+<div className="card card-2">
+<img id="cardIMG"  src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1746446441/Richard_Millie_i4yrqd.webp" className="card-img-top" alt="..."/>
 <div className="card-body">
-<h5 className="card-title">MEALS</h5>
-<p className="card-text">This card has supporting text below as a natural lead-in to additional content Lorem, ipsum dolor sit amet consectetur adipisicing el.</p>
+<h5 className="card-title">RICHARD MILLIE</h5>
+<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
 
 <div id="crossSpan">
 <div id="">
-<span className="card-text text-body-secondary">$50     </span> 
-<span id="crosstext2" className="card-text text-body-secondary">   $120</span>
+<span className="card-text text-body-secondary">$550,000     </span> 
+<span id="crosstext2" className="card-text text-body-secondary">  </span>
 </div>
 
 <div id="crossbtn-container">
-<button id="crossbtn">Add to Cart</button>
+<button id="crossbtn2">Add to Cart</button>
 </div>
 </div>
 
 </div>
 </div>
 
-<div className="card">
-<img src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1745790841/furniture_knfeln.png" className="card-img-top" alt="..."/>
+<div className="card card-2">
+<img id="cardIMG" src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1746439050/Black-Jacket_ajhhuc.webp" className="card-img-top" alt="..."/>
 <div className="card-body">
-<h5 className="card-title">FURNITURE</h5>
+<h5 className="card-title">BLACK SHIRT</h5>
 <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
 
 <div id="crossSpan">
 <div id="">
 <span className="card-text text-body-secondary">$900     </span> 
-<span id="crosstext2" className="card-text text-body-secondary">   $1,200</span>
+<span id="crosstext2" className="card-text text-body-secondary"> </span>
 </div>
 
 <div id="crossbtn-container">
-<button id="crossbtn">Add to Cart</button>
+<button id="crossbtn2">Add to Cart</button>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+{/* <div>
+  <div>
+    <h2>Meet Our Partners</h2>
+  </div>  
+
+  <div>
+    <img src="" alt="" />
+    <div>
+      h1
+    </div>
+  </div>
+</div> */}
+
+
+<div className="card-group">
+<div className="card card-2">
+<img id="cardIMG"  src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1746439060/bag_rcpwwa.webp" className="card-img-top" alt="..."/>
+<div className="card-body">
+<h5 className="card-title">LEATHER BROWN BAG</h5>
+<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+
+<div id="crossSpan">
+<div id="">
+<span className="card-text text-body-secondary">$1,100     </span> 
+<span id="crosstext2" className="card-text text-body-secondary">   </span>
+</div>
+
+<div id="crossbtn-container">
+<button id="crossbtn2">Add to Cart</button>
 </div>
 </div>
 
 </div>
 </div>
 
-{/* <div id="page3">
-<div style="font-size:2em; margin-left: 70px; margin-top: 30px;">Meet the Instructors</div>
-                  <div id="object1">
-                   <img src="workpiece/ins_justin_cutroni_1x.jpg" alt="">
-                   <h2 id="text4" >JUSTIN CUTRONI</h2><hr>
-                  <p style="margin-left: 230px; width: 800px;color: rgb(37,37,37);">Justin's mission is to teach everyone how to use digital analytics
-                                for their business. With over ten years of industry experience, Justin now leads the Google 
-                                Analytics Education team. Justin is an active participant in the digital analytics community.
-                                He publishes the blog Analytics Talk and has authored or co-authored three books on Google Analytics.
-                                In 2014, he was named the Digital Analytics Industry's
-                                Most Influential Contributor by the Digital Analytics Association.</p>
-                   </div>
+<div className="card card-2">
+<img id="cardIMG"  src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1746438951/short_d2qe3m.webp" className="card-img-top" alt="..."/>
+<div className="card-body">
+<h5 className="card-title">SHORT TROUSER</h5>
+<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
 
-                           <div id="object1">
-                               <img src="workpiece/ins_krista_seiden_1x.jpg" alt="">
-                               <h2 id="text4">KRISTA SEIDEN</h2><hr>
-                               <p style="margin-left: 230px; width: 800px; color: rgb(37,37,37);">Krista is a leader in digital analytics,
-                                advocating for best practices, and a frequent speaker at industry events. Krista co-chairs 
-                                the San Francisco chapter of the Digital Analytics Association (DAA) and mentors for the Analysis Exchange.
-                                She won the 2014 DAA Rising Star Award and the 2015 DAA Practitioner of the Year Award.</p>
-                           </div>
+<div id="crossSpan">
+<div id="">
+<span className="card-text text-body-secondary">$500     </span> 
+<span id="crosstext2" className="card-text text-body-secondary">  </span>
+</div>
 
-                           <div id="object1">
-                               <img src="workpiece/ins_ashish_vij_1x.jpg" alt="">
-                               <h2 id="text4"> ASHISH VIJ</h2><hr>
-                               <p style="margin-left: 230px; width: 800px; color: rgb(37,37,37);">Ashish Vij is a Senior Specialist on Google
-                                Analytics 360 at Google. With over 12 years of experience, he works alongside Google’s
-                                advertising teams to design and implement measurement strategies for Google’s top tier
-                                clients. He was also responsible for developing key platform features along with 
-                                deploying commercialization efforts for Google Analytics. Ashish is a measurement evangelist, 
-                                and speaks to companies around the importance of implementing measurement as a focus for advertising.</p>
-                           </div>
+<div id="crossbtn-container">
+<button id="crossbtn2">Add to Cart</button>
+</div>
+</div>
 
- </div> */}
+</div>
+</div>
+
+<div className="card card-2">
+<img id="cardIMG" src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1746448132/BLUE_SHIRT_virjdv.webp" className="card-img-top" alt="..."/>
+<div className="card-body">
+<h5 className="card-title">NAVY BLUE T-SHIRT</h5>
+<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+
+<div id="crossSpan">
+<div id="">
+<span className="card-text text-body-secondary">$900     </span> 
+<span id="crosstext2" className="card-text text-body-secondary"> </span>
+</div>
+
+<div id="crossbtn-container">
+<button id="crossbtn2">Add to Cart</button>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+<div className="card-group">
+<div className="card card-2">
+<img id="cardIMG"  src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1746446415/fadeShirt_fwbtvv.webp" className="card-img-top" alt="..."/>
+<div className="card-body">
+<h5 className="card-title">T-SHIRT</h5>
+<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+
+<div id="crossSpan">
+<div id="">
+<span className="card-text text-body-secondary">$1,500     </span> 
+<span id="crosstext2" className="card-text text-body-secondary">   </span>
+</div>
+
+<div id="crossbtn-container">
+<button id="crossbtn2">Add to Cart</button>
+</div>
+</div>
+
+</div>
+</div>
+
+<div className="card card-2">
+<img id="cardIMG"  src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1746446429/glasses_bcocxk.webp" className="card-img-top" alt="..."/>
+<div className="card-body">
+<h5 className="card-title">GLASSES</h5>
+<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+
+<div id="crossSpan">
+<div id="">
+<span className="card-text text-body-secondary">$550,000     </span> 
+<span id="crosstext2" className="card-text text-body-secondary">  </span>
+</div>
+
+<div id="crossbtn-container">
+<button id="crossbtn2">Add to Cart</button>
+</div>
+</div>
+
+</div>
+</div>
+
+<div className="card card-2">
+<img id="cardIMG" src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1746446407/Baggy_cyi2mg.webp" className="card-img-top" alt="..."/>
+<div className="card-body">
+<h5 className="card-title">BLACK BAG</h5>
+<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+
+<div id="crossSpan">
+<div id="">
+<span className="card-text text-body-secondary">$900     </span> 
+<span id="crosstext2" className="card-text text-body-secondary"> </span>
+</div>
+
+<div id="crossbtn-container">
+<button id="crossbtn2">Add to Cart</button>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+
 
 
 </div>
+
+
+
+
+
       <hr/>
       <Footer/>
       <hr/>
